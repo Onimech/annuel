fca8b0aca6b6efc6fb0a8e8b74c526c5e745f996
Onimech <117900730+Onimech@users.noreply.github.com>
2023-02-23 13:07:33 +0100

    Add files via upload

--- joueurs.php
+++ joueurs.php
@@ -3,10 +3,12 @@
 <html>
 <head>
 <meta charset="UTF8" />
-<title> Utilisateurs </title>
-<link rel="stylesheet" media="screen" href="style.css">
+<title> Liste des joueurs </title>
+<link rel="stylesheet" href="mise_en_page.css">
+</style>
 </head>
 <body> 
+    
     <ul>
     <li> <a href="index.php">Accueil</a> </li>
             <li> <a href="presentation.php">C'est quoi les Playoff de la NBA?</a> </li>
@@ -16,48 +18,59 @@
             <li> <a href="joueurs.php">Liste Joueurs</a> </li>
             <li> <a href="coachs.php">Liste Coachs</a> </li>
     </ul>
-    <h1> <center>Liste des utilisateurs</center></h1>
+    <h1> <center>Liste des Joueurs</center></h1>
 
-    <div class="listUser">
+    <div class="listJoueur">
         
     <?php
 
     //Connexion
     $user = 'root';
     $password = 'root';
-    $db = 'oiseaux';
+    $db = 'nba_projet';
     $host = 'localhost';
  
     $connect = mysqli_connect($host, $user, $password, $db);
-
+//si erreur
     if(mysqli_connect_errno())
         echo "Fail to connect :".mysqli_connect_errno();
 
     //Ecriture de la requête 
-    $requete="SELECT photo, nomCommun FROM oiseau;";
+    $requete="SELECT * FROM `joueur`;";
 
     //Envoi de la requête
     $reponse = mysqli_query($connect,$requete);
 
+    //premiere colonne tableau
     echo "<table border>";
     echo "<tr>";
-    echo "<td><h2>nomCommun</h2></td>";
-    echo "<td><h2>photo</h2></td>";
+    echo "<td><h2>Nom</h2></td>";
+    echo "<td><h2>Prénom</h2></td>";
+    echo "<td><h2>Âge</h2></td>";
+    echo "<td><h2>Numéro maillot</h2></td>";
+    echo "<td><h2>Poste</h2></td>";
+    echo "<td><h2>Nombre de Titres</h2></td>";
+    echo "<td><h2>Position Draft</h2></td>";
+    echo "<td><h2>Année de Draft</h2></td>";
+    echo "<td><h2>Equipe</h2></td>";
 
     echo "</tr>";
-
+//données du tableau
     while($ligne = mysqli_fetch_array($reponse))
     {
         //Affichage des lignes de données, champ par champ
         echo "<tr>";
-        echo "<td><h3>".$ligne['nomCommun']."</h3></td>";
-        echo "<td><h3>".$ligne['photo']."</h3></td>";
+        echo "<td><h3>".$ligne['nomJoueur']."</h3></td>";
+        echo "<td><h3>".$ligne['prenomJoueur']."</h3></td>";
+        echo "<td><h3>".$ligne['age']."</h3></td>";
+        echo "<td><h3>".$ligne['nMaillot']."</h3></td>";
+        echo "<td><h3>".$ligne['Poste']."</h3></td>";
+        echo "<td><h3>".$ligne['nTitres']."</h3></td>";
+        echo "<td><h3>".$ligne['PosDraft']."</h3></td>";
+        echo "<td><h3>".$ligne['AnDraft']."</h3></td>";
+        echo "<td><h3>".$ligne['IdEquipe']."</h3></td>";
+//comment récuperer le nom de l'équpe et pas l'id ??
 
-if ($ligne['photo']) 
-            echo"<td><img src=images/photosUsers/".$ligne['photo']." height=40px width =40px> </td>";
-else
-            echo "<td><img src=images/photosUsers/anonyme.png height=40px width=40px> </td>";
-        echo "</tr>";
     }
     echo "</table>";
 
